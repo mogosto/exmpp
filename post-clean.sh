4f2ffb420e6f137b448ef30e0d68b665551b6237
Ali Sabil <ali.sabil@tillitech.com>
2011-02-04 13:54:33 +0100

    Updated rebar and rebar.config to use pre_script and post_script

---
+++ post-clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm include/internal/exmpp_known_nss.hrl
+rm include/internal/exmpp_known_nss.hrl
+rm include/internal/exmpp_known_elems.hrl
+rm include/internal/exmpp_known_attrs.hrl
+
+rm src/core/exmpp_known_nss.erl
+rm src/core/exmpp_known_elems.erl
+rm src/core/exmpp_known_attrs.erl
